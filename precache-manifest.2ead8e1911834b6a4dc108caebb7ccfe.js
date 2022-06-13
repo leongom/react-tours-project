ee2820d841c87457c3477d6c2ddfd28f68149f12
Leon Lima <85362736+leongom@users.noreply.github.com>
2022-06-13 19:26:52 -0300

    Add files via upload

---
+++ precache-manifest.2ead8e1911834b6a4dc108caebb7ccfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c31ff17a9472a49d85b02fe1418e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad3f9a6b5c7e0beb5c7",
+    "url": "/static/css/main.8e3de9ad.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad3f9a6b5c7e0beb5c7",
+    "url": "/static/js/main.1159f3ff.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
